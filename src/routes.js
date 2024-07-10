9454d03bc8f0a60487ee9bf37940a75ba4294e82
prsousa8 <prsousa.128@gmail.com>
2024-07-10 14:14:20 -0300

    feat: adição de rotas

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,21 @@
-import Banner from "./components/Banner";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Rodape from "./components/Rodape";
-import SectionCategorias from "./components/SectionCategorias";
+import Inicio from "./pages/Inicio";
+import NovoVideo from "./pages/NovoVideo";
 
 function AppRoutes() {
   return (
     <>
+      <BrowserRouter>
       <Header/>
-      <Banner />
-      <SectionCategorias/>
-      <Rodape/>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/novo-video" element={<NovoVideo />}/>
+          <Route path="/*" element={<h1>Página não encontrada</h1>}/>
+        </Routes>
+        <Rodape/>
+      </BrowserRouter>
     </>
   );
 }
